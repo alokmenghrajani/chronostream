600b605f733e934174ceedf3a56c4fb4520798e1
Alok Menghrajani <alok@squareup.com>
2016-10-20 09:53:50 -0700

    Save keys to keystore.

--- src/main/java/chronostream/common/crypto/Crypto.java
+++ src/main/java/chronostream/common/crypto/Crypto.java
@@ -1,16 +1,29 @@
 package chronostream.common.crypto;
 
+import com.google.common.collect.ImmutableList;
+import java.io.FileOutputStream;
+import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
+import java.security.KeyStore.PasswordProtection;
+import java.security.PrivateKey;
 import java.security.Provider;
 import java.security.Security;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
 import java.security.spec.RSAKeyGenParameterSpec;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import org.bouncycastle.jce.X509Principal;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
 
 import static java.lang.String.format;
 
@@ -19,8 +32,8 @@ public class Crypto {
   private Provider provider;
   private Hkdf hkdf;
   private SecretKey hmacKey;
-  private SecretKey aesKey128, aesKey256;
-  private KeyPair rsaKey;
+  private SecretKey aes128Key, aes256Key;
+  private KeyPair rsa1024Key, rsa2048Key;
 
   public Crypto(CryptoConfig config) throws Exception {
     this.config = config;
@@ -48,41 +61,73 @@ public class Crypto {
     keyStore.load(null, config.pass());
 
     // In some cases, the keystore isn't really empty (if the Provider decides to load keys from the HSM).
-    if (keyStore.containsAlias("hmacKey")) {
-      keyStore.deleteEntry("hmacKey");
+    List<String> keysToDelete = ImmutableList.of("hmacKey", "aes128Key", "aes256Key",
+        "rsa1024Key", "rsa2048Key", "importedHmacKey", "importedAes128Key", "importedAes256Key",
+        "importedRsa1024Key", "importedRsa2048Key");
+    for (String keyToDelete: keysToDelete) {
+      if (keyStore.containsAlias(keyToDelete)) {
+        keyStore.deleteEntry(keyToDelete);
+      }
     }
-    if (keyStore.containsAlias("aesKey")) {
-      keyStore.deleteEntry("aesKey");
-    }
+
+    // save the keys so we can confirm they exist/persisted
+    PasswordProtection protectionParameter = new PasswordProtection(config.pass());
 
     // create a HmacSHA256 key, 32 bytes
     hkdf = new Hkdf(provider);
     KeyGenerator keyGen = KeyGenerator.getInstance(hmacAlg, provider);
     keyGen.init(256);
     hmacKey = keyGen.generateKey();
+    keyStore.setEntry("hmacKey", new KeyStore.SecretKeyEntry(hmacKey), protectionParameter);
 
-    // create an AES key
+    // create AES keys
     Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding", provider);
     int maxAllowedKeyLength = aesCipher.getMaxAllowedKeyLength("AES/CBC/PKCS5Padding");
     System.out.println(format("Provider: %s, maxAllowedKeyLength: %d", config.name, maxAllowedKeyLength));
 
     keyGen = KeyGenerator.getInstance("AES", provider);
     keyGen.init(128);
-    aesKey128 = keyGen.generateKey();
+    aes128Key = keyGen.generateKey();
+    keyStore.setEntry("aes128Key", new KeyStore.SecretKeyEntry(aes128Key), protectionParameter);
 
     keyGen = KeyGenerator.getInstance("AES", provider);
     keyGen.init(256);
-    aesKey256 = keyGen.generateKey();
+    aes256Key = keyGen.generateKey();
+    keyStore.setEntry("aes256Key", new KeyStore.SecretKeyEntry(aes256Key), protectionParameter);
 
-    // create RSA key
-    // note: JCE doesn't support rsa out of the box?
+    // create RSA keys (note: JCE doesn't support rsa?)
     if (!provider.getClass().getName().equals("com.sun.crypto.provider.SunJCE")) {
       KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", provider);
-      RSAKeyGenParameterSpec rsaKeyGenParameterSpec =
-          new RSAKeyGenParameterSpec(2048, RSAKeyGenParameterSpec.F4);
+      RSAKeyGenParameterSpec rsaKeyGenParameterSpec = new RSAKeyGenParameterSpec(1024, RSAKeyGenParameterSpec.F4);
       generator.initialize(rsaKeyGenParameterSpec);
-      rsaKey = generator.generateKeyPair();
+      rsa1024Key = generator.generateKeyPair();
+
+      generator = KeyPairGenerator.getInstance("RSA", provider);
+      rsaKeyGenParameterSpec = new RSAKeyGenParameterSpec(2048, RSAKeyGenParameterSpec.F4);
+      generator.initialize(rsaKeyGenParameterSpec);
+      rsa2048Key = generator.generateKeyPair();
+
+      keyStore.setEntry("rsa1024Key", new KeyStore.PrivateKeyEntry(rsa1024Key.getPrivate(), new Certificate[]{generateCertificate(rsa1024Key)}), protectionParameter);
+      keyStore.setEntry("rsa2048Key", new KeyStore.PrivateKeyEntry(rsa2048Key.getPrivate(), new Certificate[]{generateCertificate(rsa2048Key)}), protectionParameter);
     }
+
+    // save the keystore
+    keyStore.store(new FileOutputStream(config.keyStore), config.pass());
+  }
+
+  public X509Certificate generateCertificate(KeyPair keyPair) throws Exception {
+    X509V3CertificateGenerator cert = new X509V3CertificateGenerator();
+    cert.setSerialNumber(BigInteger.valueOf(1));
+    cert.setSubjectDN(new X509Principal("CN=localhost"));
+    cert.setIssuerDN(new X509Principal("CN=localhost"));
+    cert.setPublicKey(keyPair.getPublic());
+    LocalDate now = LocalDate.now();
+    cert.setNotBefore(Date.from(now.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+    now = now.plusDays(300);
+    cert.setNotAfter(Date.from(now.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+    cert.setSignatureAlgorithm("SHA512withRSA");
+    PrivateKey signingKey = keyPair.getPrivate();
+    return cert.generate(signingKey, provider.getName());
   }
 
   /**
@@ -110,9 +155,10 @@ public class Crypto {
     }
 
     this.hmacKey = new SecretKeySpec(importFrom.hmacKey.getEncoded(), hmacAlg);
-    this.aesKey128 = new SecretKeySpec(importFrom.aesKey128.getEncoded(), "AES");
-    this.aesKey256 = new SecretKeySpec(importFrom.aesKey256.getEncoded(), "AES");
-    this.rsaKey = importFrom.rsaKey;
+    this.aes128Key = new SecretKeySpec(importFrom.aes128Key.getEncoded(), "AES");
+    this.aes256Key = new SecretKeySpec(importFrom.aes256Key.getEncoded(), "AES");
+    this.rsa1024Key = importFrom.rsa1024Key;
+    this.rsa2048Key = importFrom.rsa2048Key;
   }
 
   public byte[] doCrypto(CryptoPrimitive primitive, byte[] buffer, byte[] iv) throws Exception {
@@ -143,13 +189,13 @@ public class Crypto {
 
   public byte[] doAesGcmEncryption(byte[] bytes, byte[] iv) throws Exception {
     Cipher cipher = Cipher.getInstance("AES/GCM/Nopadding", provider);
-    cipher.init(Cipher.ENCRYPT_MODE, aesKey128, new IvParameterSpec(iv));
+    cipher.init(Cipher.ENCRYPT_MODE, aes128Key, new IvParameterSpec(iv));
     return cipher.doFinal(bytes);
   }
 
   public byte[] doAesGcmDecryption(byte[] bytes, byte[] iv) throws Exception {
     Cipher cipher = Cipher.getInstance("AES/GCM/Nopadding", provider);
-    cipher.init(Cipher.DECRYPT_MODE, aesKey128, new IvParameterSpec(iv));
+    cipher.init(Cipher.DECRYPT_MODE, aes128Key, new IvParameterSpec(iv));
     return cipher.doFinal(bytes);
   }
 
@@ -157,13 +203,13 @@ public class Crypto {
 
   public byte[] doAesCbcEncryption(byte[] bytes, byte[] iv) throws Exception {
     Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", provider);
-    cipher.init(Cipher.ENCRYPT_MODE, aesKey256, new IvParameterSpec(iv));
+    cipher.init(Cipher.ENCRYPT_MODE, aes256Key, new IvParameterSpec(iv));
     return cipher.doFinal(bytes);
   }
 
   public byte[] doAesCbcDecryption(byte[] bytes, byte[] iv) throws Exception {
     Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", provider);
-    cipher.init(Cipher.DECRYPT_MODE, aesKey256, new IvParameterSpec(iv));
+    cipher.init(Cipher.DECRYPT_MODE, aes256Key, new IvParameterSpec(iv));
     return cipher.doFinal(bytes);
   }
 
@@ -171,13 +217,13 @@ public class Crypto {
 
   public byte[] doRsaEncryption(byte[] bytes) throws Exception {
     Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding", provider);
-    cipher.init(Cipher.ENCRYPT_MODE, rsaKey.getPublic());
+    cipher.init(Cipher.ENCRYPT_MODE, rsa2048Key.getPublic());
     return cipher.doFinal(bytes);
   }
 
   public byte[] doRsaDecryption(byte[] bytes) throws Exception {
     Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding", provider);
-    cipher.init(Cipher.DECRYPT_MODE, rsaKey.getPrivate());
+    cipher.init(Cipher.DECRYPT_MODE, rsa2048Key.getPrivate());
     return cipher.doFinal(bytes);
   }
 }
